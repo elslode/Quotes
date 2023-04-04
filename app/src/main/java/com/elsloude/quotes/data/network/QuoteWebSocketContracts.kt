f13e9f9d8351a27f9bc71c6a7400ea29941685fd
Elisey <63147307+elslode@users.noreply.github.com>
2023-04-04 11:47:08 +0200

    Revert "Presentation layer"

--- app/src/main/java/com/elsloude/quotes/data/network/QuoteWebSocketContracts.kt
+++
@@ -1,8 +0,0 @@
-package com.elsloude.quotes.data.network
-
-import kotlinx.coroutines.flow.Flow
-
-interface QuoteWebSocketContracts {
-    val quoteStateFlow: Flow<WebSocketStateDto>
-    fun setQuote(response: WebSocketStateDto)
-}
